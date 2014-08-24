963634e7874a6ecaad48875d81e56480ee0c13fa
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2014-08-24 13:53:11 +0300

    Added new group for entity serializers, added a base file for serializers

---
+++ Engine/Entities/Serializers/BaseEntitySerializer.cpp
@@ -0,0 +1,17 @@
+#include "Include.h"
+// ------------------------------------ //
+#ifndef LEVIATHAN_BASEENTITYSERIALIZER
+#include "BaseEntitySerializer.h"
+#endif
+using namespace Leviathan;
+using namespace Leviathan::Gui;
+// ------------------------------------ //
+
+
+// ------------------------------------ //
+
+// ------------------------------------ //
+
+// ------------------------------------ //
+
+// ------------------------------------ //

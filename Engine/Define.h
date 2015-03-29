580fb0484a6761b831646291e2ee23c0f5e6c250
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2015-03-29 15:44:19 +0300

    Added defines for different network strategies, removed unnecessary object methods

--- Engine/Define.h
+++ Engine/Define.h
@@ -14,6 +14,20 @@
 //! When true entities may run a single physical update with a short timestep when resimulating
 //#define ALLOW_RESIMULATE_CONSUME_ALL
 
+//! Defines the networking mode
+//! In this mode the server sends snapshots of moving entities to all clients which then interpolate
+//! between states. Input will not be replicated on all clients. Clients locally simulate their own
+//! inputs. Server uses resimulation to simulate clients taking actions in the past.
+#define NETWORK_USE_SNAPSHOTS
+
+#ifndef NETWORK_USE_SNAPSHOTS
+//! In this mode all clients run the whole simulation with access to all inputs from all clients.
+//! The server then sends verification snapshots to clients who then resimulate and interpolate if
+//! their results were different.
+#define NETWORK_USE_RESIMULATE
+#endif //NETWORK_USE_SNAPSHOTS
+
+
 namespace Leviathan{
 
 	template<class T>
@@ -29,11 +43,8 @@ namespace Leviathan{
 	public:
 		DLLEXPORT Object();;
 		DLLEXPORT virtual ~Object();
-		DLLEXPORT virtual bool IsThis(Object* compare);
-
-	protected:
-
 	};
+    
 	// has no virtual destructor, objects may not be pointed by this base class //
 	class EngineComponent : public Object{
 	public:
@@ -41,12 +52,6 @@ namespace Leviathan{
 
 		DLLEXPORT virtual bool Init();
 		DLLEXPORT virtual void Release();
-		DLLEXPORT inline bool IsInited(){
-			return Inited;
-		}
-	protected:
-		bool Inited;
-
 	};
 }
 

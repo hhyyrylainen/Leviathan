58867c196b72f3f07674b7a02c0bbb3b97a59d10
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2016-06-17 21:56:38 +0300

    Added .desktop file generation for linux, almost everything now gets properly copied, except cAudio

--- CreateLinuxIcon.rb
+++ CreateLinuxIcon.rb
@@ -19,8 +19,18 @@ OptionParser.new do |opts|
   opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
     options[:verbose] = v
   end
+  opts.on("-n", "--name [NAME]", "Specify name to use in .desktop") do |name|
+    options[:name] = name
+  end
+  opts.on("-p", "--prefix [PREFIX]", "Prefix to add to icon") do |prefix|
+    options[:prefix] = prefix
+  end
+  opts.on("-t", "--terminal", "Specify Terminal in .dekstop") do |term|
+    options[:terminal] = term
+  end
 end.parse!
-p options
+
+options[:terminal] = false if not options[:terminal]
 
 if ARGV.count < 1
   onError "No file provided"
@@ -29,6 +39,8 @@ end
 if ARGV.count > 2
   onError "Too many arguments provided"
 end
+
+onError "No name provided" if not options[:name]
 
 targetExecutable = ARGV[0]
 
@@ -39,7 +51,7 @@ if ARGV.count > 1
   useIcon = ARGV[1]
   info "Using icon #{useIcon}" if options[:verbose]
 else
-  useIcon = Path.join(CurrentDir, "LeviathanIcon.bmp")
+  useIcon = File.join(CurrentDir, "LeviathanIcon.bmp")
 end
 
 onError "Icon file: " + useIcon + " doesn't exist" if not File.exist? useIcon
@@ -47,7 +59,55 @@ onError "Icon file: " + useIcon + " doesn't exist" if not File.exist? useIcon
 targetPath = Pathname.new(targetExecutable)
 iconPath = Pathname.new(useIcon)
 
+if iconPath.extname != ".png"
 
-info "Creating  for #{targetExecutable} in folder #{targetPath.dirname}" if options[:verbose]
+  info "Icon of extension '#{iconPath.extname}' will be converted to .png" if options[:verbose]
+  doConvert = true
+end
+
+if options[:verbose]
+  info "Creating .desktop for #{targetExecutable} in folder #{targetPath.dirname}"
+end
+
+targetDesktop = File.join(targetPath.dirname, "#{targetPath.basename}.desktop")
+
+finalIconFile = File.join(targetPath.dirname,
+                          "#{options[:prefix]}#{iconPath.basename(iconPath.extname)}.png")
+
+if not File.exist? finalIconFile
+
+  if doConvert
+
+    if options[:verbose]
+      info "Converting icon #{useIcon} -> #{finalIconFile}"
+    end
+    
+    system "convert \"#{useIcon}[0]\" \"#{finalIconFile}\""
+    onError "Failed to convert icon file to png" if $?.exitstatus > 0
+  else
+    FileUtils.cp useIcon, finalIconFile
+  end
+end
+
+contents = %{
+[Desktop Entry]
+Encoding=UTF-8
+Version=1.0
+Type=Application
+Terminal=#{options[:terminal].to_s}
+Exec=./#{targetPath.basename}
+Name=#{options[:name]}
+Icon=./#{File.basename(finalIconFile)}
+Path=./
+}
+
+File.write(targetDesktop, contents)
+
+if options[:verbose]
+  success "Created #{targetDesktop}"
+end
+exit 0
+
+       
 
 

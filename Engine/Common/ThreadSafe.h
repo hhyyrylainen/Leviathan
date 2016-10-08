db373ebc76402df11366f6140e78bca919ee9466
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2016-10-08 14:05:29 +0300

    Made GUARD_LOCK macros use Leviathan namespace when creating locks

--- Engine/Common/ThreadSafe.h
+++ Engine/Common/ThreadSafe.h
@@ -79,23 +79,23 @@ namespace Leviathan{
     
 #if 0
     // These prevent copy elision
-#define GUARD_LOCK() auto guard = std::move(Locker::Object(this));
+#define GUARD_LOCK() auto guard = std::move(Leviathan::Locker::Object(this));
     
-#define GUARD_LOCK_OTHER(x) auto guard = std::move(Locker::Object(x));
-#define GUARD_LOCK_NAME(y) auto y = std::move(Locker::Object(this));
-#define GUARD_LOCK_OTHER_NAME(x,y) auto y = std::move(Locker::Object(x));
+#define GUARD_LOCK_OTHER(x) auto guard = std::move(Leviathan::Locker::Object(x));
+#define GUARD_LOCK_NAME(y) auto y = std::move(Leviathan::Locker::Object(this));
+#define GUARD_LOCK_OTHER_NAME(x,y) auto y = std::move(Leviathan::Locker::Object(x));
     
-#define UNIQUE_LOCK_OBJECT_OTHER(x) auto lockit = std::move(Locker::Object(x));
-#define UNIQUE_LOCK_THIS() auto lockit = std::move(Locker::Object(this));
+#define UNIQUE_LOCK_OBJECT_OTHER(x) auto lockit = std::move(Leviathan::Locker::Object(x));
+#define UNIQUE_LOCK_THIS() auto lockit = std::move(Leviathan::Locker::Object(this));
 #else
-#define GUARD_LOCK() auto guard = (Locker::Object(this));
+#define GUARD_LOCK() auto guard = (Leviathan::Locker::Object(this));
     
-#define GUARD_LOCK_OTHER(x) auto guard = (Locker::Object(x));
-#define GUARD_LOCK_NAME(y) auto y = (Locker::Object(this));
-#define GUARD_LOCK_OTHER_NAME(x,y) auto y = (Locker::Object(x));
+#define GUARD_LOCK_OTHER(x) auto guard = (Leviathan::Locker::Object(x));
+#define GUARD_LOCK_NAME(y) auto y = (Leviathan::Locker::Object(this));
+#define GUARD_LOCK_OTHER_NAME(x,y) auto y = (Leviathan::Locker::Object(x));
     
-#define UNIQUE_LOCK_OBJECT_OTHER(x) auto lockit = (Locker::Object(x));
-#define UNIQUE_LOCK_THIS() auto lockit = (Locker::Object(this));
+#define UNIQUE_LOCK_OBJECT_OTHER(x) auto lockit = (Leviathan::Locker::Object(x));
+#define UNIQUE_LOCK_THIS() auto lockit = (Leviathan::Locker::Object(this));
 #endif
     
 	//! \brief Allows the inherited object to be locked

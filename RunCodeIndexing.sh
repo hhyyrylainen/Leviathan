d78f1d0fc4b7f31a0156553a576556c690fbe367
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2017-06-26 20:13:40 +0300

    Initial update to move files to the RubySetupSystem

--- RunCodeIndexing.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#cscope -b -q
-find . -iname "*.h" -o -iname "*.cpp" -not -path "./build/*" > cscope.files
-cscope -b
-
--- Setup.rb
+++ Setup.rb
@@ -8,9 +8,18 @@
 # Ogre required libs: rapidjson-devel
 # compile flags: OGRE_CONFIG_THREADS=2 (background resources) OGRE_CONFIG_THREAD_PROVIDER=std
 
+# RubySetupSystem Bootstrap
+system "git submodule init && git submodule update"
+
+if $?.exitstatus != 0
+  abort("Failed to initialize or update git submodules. " +
+        "Please make sure git is in path and " +
+        "you have an ssh key setup for your github account")
+end
+
 require 'fileutils'
 
-require_relative 'Helpers/RubyCommon.rb'
+require_relative 'RubySetupSystem/RubyCommon.rb'
 
 def checkRunFolder(suggested)
 
@@ -18,17 +27,26 @@ def checkRunFolder(suggested)
 
     onError("Not ran from Leviathan base directory!") if not File.exist?(doxyFile)
 
-    return File.expand_path("..", suggested)
+    thirdPartyFolder = suggested, "ThirdParty"
+
+    FileUtils.mkdir_p thirdPartyFolder
+    FileUtils.mkdir_p File.join suggested, "build", "ThirdParty"
+    
+    thirdPartyFolder
     
 end
 
 def projectFolder(baseDir)
 
-  return File.join baseDir, "leviathan"
+  File.expand_path File.join(baseDir, "../")
   
 end
 
-require_relative 'Helpers/RubySetupSystem.rb'
+def parseExtraArgs
+
+end
+
+require_relative 'RubySetupSystem/RubySetupSystem.rb'
 
 # If false won't get breakpad
 GetBreakpad = true
@@ -37,85 +55,67 @@ GetBreakpad = true
 FetchAssets = true
 
 
+# Setup dependencies settings
+THIRD_PARTY_INSTALL = File.join(ProjectDir, "build", "ThirdParty")
+
+newton = Newton.new(
+  version: "master",
+  installPath: THIRD_PARTY_INSTALL,
+  noInstallSudo: true
+)
+
+angelscript = AngelScript.new(
+  version: "2.31.2",
+  installPath: THIRD_PARTY_INSTALL,
+  noInstallSudo: true
+)
+
+sfml = SFML.new(
+  version: "2.4.x",
+  installPath: THIRD_PARTY_INSTALL,
+  noInstallSudo: true
+)
+
+ogre = Ogre.new(
+  version: "2.4.x",
+  installPath: THIRD_PARTY_INSTALL,
+  noInstallSudo: true
+)
+
+cegui = CEGUI.new(
+  # TODO: once my pull request is accepted put it here
+  version: "master",
+  installPath: THIRD_PARTY_INSTALL,
+  extraSearchPath: THIRD_PARTY_INSTALL,
+  noInstallSudo: true
+)
+
 # All the objects
-installer = Installer.new(Array[Newton.new, AngelScript.new, SFML.new, Ogre.new,
-                                CEGUIDependencies.new, CEGUI.new])
+installer = Installer.new(
+  [newton, angelscript, sfml, ogre, cegui]
+)
 
 if GetBreakpad
 
-  installer.addLibrary Breakpad.new
+  breakpad = Breakpad.new(
+    version: "master",
+    installPath: THIRD_PARTY_INSTALL,
+    noInstallSudo: true
+  )
+  
+  installer.addLibrary 
 
 end
 
 installer.run
 
-# Library windows hackery
-# TODO: move this into installer.run
-if not OnlyMainProject
-
-  if BuildPlatform == "windows"
-
-    onError "todo: update this stuff"
-
-    Dir.chdir(File.join(CurrentDir, "leviathan")) do
-      
-      # Make sure Ogre home is set
-      ENV["OGRE_HOME"] = "#{CurrentDir}/../ogre/build/sdk"
-      ENV["OIS_HOME"] = "#{CurrentDir}/../ogre/build/sdk"
-      
-      info "Moving all the libraries to leviathan/Windows/ThirdParty"
-      FileUtils.mkdir_p "Windows/ThirdParty"
-      FileUtils.mkdir_p "Windows/ThirdParty/lib"
-      FileUtils.mkdir_p "Windows/ThirdParty/bin"
-      FileUtils.mkdir_p "Windows/ThirdParty/include"
-      
-      Dir.chdir("Windows/ThirdParty/include") do
-        FileUtils.copy_entry "#{CurrentDir}/../cAudio/build/Install/include/cAudio", "cAudio"
-        FileUtils.copy_entry "#{CurrentDir}/../cegui-dependencies/src/glm-0.9.4.5/glm", "glm"
-        FileUtils.copy_entry "#{CurrentDir}/../cegui/cegui/include/CEGUI", "CEGUI"
-        FileUtils.cp_r(Dir.glob("#{CurrentDir}/../cegui/build/cegui/include/CEGUI/*"), "CEGUI")
-        FileUtils.cp_r(Dir.glob("#{CurrentDir}/../ogre/build/sdk/include/*"), "./")
-        FileUtils.copy_entry "#{CurrentDir}/../SFML/include/SFML", "SFML"
-      end
-      
-      copyStuff ".lib", "Windows/ThirdParty/lib"
-      copyStuff ".dll", "Windows/ThirdParty/bin"
-      
-      runGlobberAndCopy(Globber.new(["sfml-network.lib", "sfml-system.lib"], "#{CurrentDir}/../SFML/build"), 
-                        "Windows/ThirdParty/lib")
-      runGlobberAndCopy(Globber.new(["sfml-network-2.dll", "sfml-system-2.dll"], "#{CurrentDir}/../SFML/build"), 
-                        "Windows/ThirdParty/bin")
-      
-      # Debug versions
-      runGlobberAndCopy(Globber.new(["sfml-network-d.lib", "sfml-system-d.lib"], "#{CurrentDir}/../SFML/build"), 
-                        "Windows/ThirdParty/lib")
-      runGlobberAndCopy(Globber.new(["sfml-network-d-2.dll", "sfml-system-d-2.dll"], "#{CurrentDir}/../SFML/build"), 
-                        "Windows/ThirdParty/bin")
-      
-      
-      runGlobberAndCopy(Globber.new(["pcre.dll", "freetype.dll"], "#{CurrentDir}/../cegui"), 
-                        "Windows/ThirdParty/bin")
-      
-      # TODO: configure this
-      runGlobberAndCopy(Globber.new(["boost_chrono-vc140-mt-1_60.dll", 
-                                     "boost_system-vc140-mt-1_60.dll", "boost_filesystem-vc140-mt-1_60.dll"], "#{CurrentDir}/../boost/stage"), 
-                        "Windows/ThirdParty/bin")
-      # And debug versions
-      runGlobberAndCopy(Globber.new(["boost_chrono-vc140-mt-gd-1_60.dll", 
-                                     "boost_system-vc140-mt-gd-1_60.dll", "boost_filesystem-vc140-mt-gd-1_60.dll"], "#{CurrentDir}/../boost/stage"), 
-                        "Windows/ThirdParty/bin")
-    end
-  end
-  
-end
-
-if not File.exist? File.join(CurrentDir, "leviathan")
+if not File.exist? ProjectDir
 
   onError "'leviathan' folder is missing"
 
 end
 
-Dir.chdir(File.join(CurrentDir, "leviathan")) do
+Dir.chdir(ProjectDir) do
   
   # Assets svn
   if FetchAssets and not SkipPullUpdates
@@ -155,10 +155,9 @@ info "Compiling Leviathan"
 
 # Build directory is made earlier
 
-Dir.chdir(File.join(CurrentDir, "leviathan", "build")) do
+Dir.chdir(File.join(ProjectDir, "build")) do
 
   runCMakeConfigure "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DUSE_BREAKPAD=OFF"
-
   
   if $?.exitstatus > 0
     onError "Failed to configure Leviathan"
@@ -174,7 +173,9 @@ success "Done compiling Leviathan"
 if BuildPlatform == "linux"
   
   info "Indexing with cscope"
-  system "./RunCodeIndexing.sh"
+  Dir.chdir(ProjectDir) do
+    systemChecked "RubySetupSystem/RunCodeIndexing.sh"
+  end
   
   success "All done."
   info "To compile run 'make' in ./build"
@@ -187,8 +188,4 @@ else
 end
 
 exit 0
-
-
-
-
 

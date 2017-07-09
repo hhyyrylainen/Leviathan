2c5d0f66c77184194f695da7122252705299f94e
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2017-07-09 14:23:07 +0300

    Added WireData definition

---
+++ Engine/Networking/WireData.h
@@ -0,0 +1,34 @@
+// Leviathan Game Engine
+// Copyright (c) 2012-2017 Henri Hyyryläinen
+#pragma once
+
+#include "CommonNetwork.h"
+
+namespace sf{
+class Packet;
+}
+
+namespace Leviathan{
+
+class SentRequest;
+class SentResponse;
+class NetworkRequest;
+class NetworkResponse;
+class SentNetworkThing;
+
+//! Class for serializing and deserializing the final bytes that are
+//! sent over the network
+//!
+//! Used by Connection and tests to format NetworkResponse and NetworkRequest objects
+class WireData final{
+public:
+
+    
+    
+protected:
+    
+
+};
+
+
+}

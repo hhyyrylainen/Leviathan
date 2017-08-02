b9b2390fedb981346e4bef14a84bfadcfe07e970
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2017-08-02 20:44:14 +0300

    Added filesystem and Ogre basic initialization to the gui test

--- LeviathanTest/TestFiles/GuiTests.cpp
+++ LeviathanTest/TestFiles/GuiTests.cpp
@@ -4,6 +4,9 @@
 #include "GUI/AlphaHitCache.h"
 #include "FileSystem.h"
 
+#include "OgreRoot.h"
+#include "OgreLogManager.h"
+
 #include "../PartialEngine.h"
 
 #include "catch.hpp"
@@ -40,10 +43,41 @@ TEST_CASE("CEGUI Image property is correctly parsed", "[gui]"){
 
     SECTION("Taharezlook imageset can be properly read"){
 
+        // TODO: allow the Graphics object to be used here
+        // Suppress log
+        Ogre::LogManager logMgr;
+
+        Ogre::Log* ogreLog = logMgr.createLog("Test/TestOgreLog.txt", true, false, false);
+
+        REQUIRE(ogreLog == logMgr.getDefaultLog());
+        
+        Ogre::Root root("", "", "");
+
+        Ogre::String renderSystemName = "RenderSystem_GL3Plus";
+
+    #ifdef _DEBUG
+        renderSystemName->append("_d");
+    #endif // _DEBUG
+
+    #ifndef _WIN32            
+        // On platforms where rpath works plugins are in the lib subdirectory
+        renderSystemName = "lib/" + renderSystemName; 
+    #endif
+        
+        root.loadPlugin(renderSystemName);
+        const auto& renderers = root.getAvailableRenderers();
+        REQUIRE(renderers.size() > 0);
+        REQUIRE(renderers[0]);
+        root.setRenderSystem(renderers[0]);
+        root.initialise(false, "", "");
+        
         FileSystem filesystem;
         TestLogger reporter("Test/gui_imageset_test.txt");
         
         REQUIRE(filesystem.Init(&reporter));
+
+        // Register resources to Ogre //
+        filesystem.RegisterOGREResourceGroups(true);
 
         AlphaHitCache cache;
 

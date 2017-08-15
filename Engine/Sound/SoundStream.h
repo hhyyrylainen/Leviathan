ab0a79ac9f6a50bce9c137c017882291e0099738
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2017-08-15 23:11:29 +0300

    Added more documentation to SoundStream

--- Engine/Sound/SoundStream.h
+++ Engine/Sound/SoundStream.h
@@ -17,7 +17,10 @@ namespace Leviathan{
 //! new audio data to play
 class SoundStream : public ThreadSafe, public sf::SoundStream{
 public:
-    
+
+    //! \param datacallback \see MoreDataCallback
+    //! \param samplerate How many samples are player per second
+    //! \param channelcount Number of audio channels
     SoundStream(std::function<bool (std::vector<int16_t>&)> datacallback,
         unsigned int channelcount, unsigned int samplerate);
 
@@ -33,6 +36,8 @@ protected:
 
     //! Callback for filling in PlayingAudioData
     //! Return value of true keeps playing. Returning false stops the playback
+    //! Needs to return a sample for all channels so vector.size() % channelcount == 0
+    //! One call should return a couple thousand samples for each channel
     std::function<bool (std::vector<int16_t>&)> MoreDataCallback;
 };
 

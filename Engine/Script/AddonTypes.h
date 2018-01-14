61bfbcb9e02081a81ce73855ceaf50e681b56fb3
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-01-14 21:08:29 +0200

    Added a pointless file that could turn useful if some advanced angelscript bindings are made

---
+++ Engine/Script/AddonTypes.h
@@ -0,0 +1,12 @@
+// Leviathan Game Engine
+// Copyright (c) 2012-2018 Henri Hyyryläinen
+#include "Include.h"
+// ------------------------------------ //
+// File with helpers for handling angelscript addon types on the c++ side
+// TODO: find a way to allow c++ side to use boost::intrusive_ptr and the script side just
+// assign normally
+#include "Common/ReferenceCounted.h"
+
+
+// #include <add_on/scriptdictionary/scriptdictionary.h>
+

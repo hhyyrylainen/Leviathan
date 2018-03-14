7c45ee3d992d944e7c5392ac3e1ccda93b6ddff9
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-03-15 00:37:08 +0200

    Previous recreation attempt didn't help

--- Engine/Entities/Components.cpp
+++ Engine/Entities/Components.cpp
@@ -485,15 +485,16 @@ DLLEXPORT void Physics::SetMass(float mass)
 
     // #ifdef CHECK_FOR_NANS
 
-    if(std::isnan(inertia.X) || std::isnan(inertia.Y) || std::isnan(inertia.Z) ||
-        std::isnan(centerofmass.X) || std::isnan(centerofmass.Y) ||
-        std::isnan(centerofmass.Z)) {
+    // Doesn't work. Apparently there is a chance to screw up the collision object permanently
+    // if(std::isnan(inertia.X) || std::isnan(inertia.Y) || std::isnan(inertia.Z) ||
+    //     std::isnan(centerofmass.X) || std::isnan(centerofmass.Y) ||
+    //     std::isnan(centerofmass.Z)) {
 
-        LOG_ERROR("Physics: SetMass: failed to calculate physics body intertial matrix, "
-                  "trying again");
-        SetMass(mass);
-        return;
-    }
+    //     LOG_ERROR("Physics: SetMass: failed to calculate collision inertial matrix, "
+    //               "trying again");
+    //     SetMass(mass);
+    //     return;
+    // }
 
     inertia.CheckForNans();
     centerofmass.CheckForNans();

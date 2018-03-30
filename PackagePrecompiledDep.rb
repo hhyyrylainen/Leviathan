9083105dfe6e50302893518aaa22edd72cf701c8
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-03-30 12:29:44 +0300

    Updated precompiled packaging to automatically get names and updated setup for new precompiled versions

--- PackagePrecompiledDep.rb
+++ PackagePrecompiledDep.rb
@@ -1,5 +1,5 @@
 # Package a leviathan dependency as precompiled binary for current platform
-# To package all run: `ruby PackagePrecompiledDep.rb newton  angelscript sfml ffmpeg zlib freeimage sdl freetype ogre cegui`
+# To package all run: `ruby PackagePrecompiledDep.rb "newton dynamics"  angelscript sfml ffmpeg zlib freeimage sdl freetype ogre cegui "openal soft" caudio`
 PrecompiledInstallFolder = "build/ThirdParty"
 
 require_relative 'RubySetupSystem/CreatePrecompiled.rb'
@@ -10,29 +10,13 @@ require_relative 'LeviathanLibraries.rb'
 def getDependencyObjectByName(name)
 
   # Could make sure this is automatically up to date
-  case name
-  when "newton"
-    return $newton
-  when "angelscript"
-    return $angelscript
-  when "sfml"
-    return $sfml
-  when "ffmpeg"
-    return $ffmpeg
-  when "ogre"
-    return $ogre
-  when "cegui"
-    return $cegui
-  # These are Windows only
-  when "zlib"
-    return $zlib
-  when "freeimage"
-    return $freeimage
-  when "sdl"
-    return $sdl
-  when "freetype"
-    return $freetype
-  end
+  $leviathanLibList.each{|lib|
+    
+    if name.casecmp(lib.Name).zero? || name.casecmp(lib.FolderName).zero?
+      return lib
+    end
+  }
+  nil
 end
 
 runPackager()

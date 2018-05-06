3c9d25e59301000e0dcc6d065895e0e1e40207ac
Henri Hyyryläinen <henri.hyyrylainen@gmail.com>
2018-05-06 18:49:53 +0300

    Added variant for calling events with intrusive_ptrs

--- Engine/Events/EventHandler.h
+++ Engine/Events/EventHandler.h
@@ -3,13 +3,13 @@
 #pragma once
 #include "Define.h"
 // ------------------------------------ //
-#include "Event.h"
 #include "CallableObject.h"
+#include "Event.h"
 
-namespace Leviathan{
+namespace Leviathan {
 
 //! \brief Allows object to register for events that can be fired from anywhere
-class EventHandler : public ThreadSafe{
+class EventHandler : public ThreadSafe {
 public:
     DLLEXPORT EventHandler();
     DLLEXPORT ~EventHandler();
@@ -19,26 +19,35 @@ public:
 
     //! \param event The event to send. Reference count will be decremented by this
     DLLEXPORT void CallEvent(Event* event);
+    DLLEXPORT inline void CallEvent(const Event::pointer& event)
+    {
+        event->AddRef();
+        CallEvent(event.get());
+    }
 
     //! \param event The event to send. Reference count will be decremented by this
     DLLEXPORT void CallEvent(GenericEvent* event);
+    DLLEXPORT inline void CallEvent(const GenericEvent::pointer& event)
+    {
+        event->AddRef();
+        CallEvent(event.get());
+    }
 
     DLLEXPORT bool RegisterForEvent(CallableObject* toregister, EVENT_TYPE totype);
-    DLLEXPORT bool RegisterForEvent(CallableObject* toregister,
-        const std::string &genericname);
-    
+    DLLEXPORT bool RegisterForEvent(
+        CallableObject* toregister, const std::string& genericname);
+
     DLLEXPORT void Unregister(CallableObject* caller, EVENT_TYPE type, bool all = false);
-    DLLEXPORT void Unregister(CallableObject* caller, const std::string &genericname,
-        bool all = false);
+    DLLEXPORT void Unregister(
+        CallableObject* caller, const std::string& genericname, bool all = false);
 
 private:
     std::vector<std::tuple<CallableObject*, EVENT_TYPE>> EventListeners;
     std::vector<std::tuple<CallableObject*, std::string>> GenericEventListeners;
 };
 
-}
+} // namespace Leviathan
 
 #ifdef LEAK_INTO_GLOBAL
 using Leviathan::EventHandler;
 #endif
-
